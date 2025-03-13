f870fd452db6f2d47430e501bfee725bde6b4f5a
root <root@DESKTOP-M3CKKGV.>
2025-03-12 21:48:27 -0500

    another one two

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,7 +68,7 @@ resource "aws_route_table" "tf_public_route_table" {
 }
 
 resource "aws_route_table_association" "tf_public_subnet_association" {
-    for_each = aws_subnet.tf_public_subnet
+    for_each = toset(aws_subnet.tf_public_subnet)
     subnet_id = each.value.id
     route_table_id = aws_route_table.tf_public_route_table.id
 }
